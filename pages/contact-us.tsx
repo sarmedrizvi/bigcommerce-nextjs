6ae5fa4f10b573585bd85822674d9ea3d55bbc5b
Sarmed <sarmedrizvi@gmail.com>
2021-05-22 20:40:43 +0500

    contact us

---
+++ pages/contact-us.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Layout } from '@components/common'
+import type { GetStaticPropsContext } from 'next'
+import CreateClient from 'util/hooks/create-client'
+import get from 'lodash/get'
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+
+interface Props {
+  data: String
+}
+
+const DeliveryInformation = ({ data }: Props) => {
+  const title = get(data, 'items[0].fields.title', '')
+  const description = get(data, 'items[0].fields.description', {})
+  return (
+    <div className="mx-3">
+      <h1 className="mb-5 text-center">{title}</h1>
+      <span>{documentToReactComponents(description)}</span>
+    </div>
+  )
+}
+
+export async function getStaticProps() {
+  const res = await CreateClient({
+    'sys.contentType.sys.id': 'deliveryInformation',
+  })
+  return {
+    props: { data: res },
+    revalidate: 14400,
+  }
+}
+
+export default DeliveryInformation
+DeliveryInformation.Layout = Layout
